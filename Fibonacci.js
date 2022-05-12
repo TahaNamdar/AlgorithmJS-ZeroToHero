7f8b816ee1c0520a30bea4ba2be5bc42d946fee2
tahanamdar30 <tahanamdar30@gmail.com>
2022-05-12 18:42:38 +0430

    fibo solution2

--- Fibonacci.js
+++ Fibonacci.js
@@ -19,3 +19,22 @@ function fib1(n) {
 }
 
 //2
+
+function fib2(n) {
+  let a = 0,
+    b = 1;
+  if (n >= 0) {
+    console.log(a);
+  }
+  if (n >= 1) {
+    console.log(b);
+  }
+  for (let i = 2; i <= n; i++) {
+    console.log(a + b);
+    b = a + b;
+    a = b - a;
+  }
+}
+
+fib2(5);
+
