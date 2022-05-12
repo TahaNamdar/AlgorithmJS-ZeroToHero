8ae022547e7c347bb6d2d19c16008573c7c89f65
tahanamdar30 <tahanamdar30@gmail.com>
2022-05-12 18:59:56 +0430

    fibo solution3

--- Fibonacci.js
+++ Fibonacci.js
@@ -10,14 +10,14 @@ function fib1(n) {
   if (n >= 1) {
     console.log(b);
   }
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i < n; i++) {
     c = a + b;
     console.log(c);
     a = b;
     b = c;
   }
 }
-
+fib1(5);
 //2
 
 function fib2(n) {
@@ -29,12 +29,18 @@ function fib2(n) {
   if (n >= 1) {
     console.log(b);
   }
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i < n; i++) {
     console.log(a + b);
     b = a + b;
     a = b - a;
   }
 }
 
-fib2(5);
-
+//3
+function fib3(n) {
+  return new Array(n).fill(1).reduce((arr, _, i) => {
+    arr.push(i <= 1 ? i : arr[i - 2] + arr[i - 1]);
+    return arr;
+  }, []);
+}
+console.log(fib3(5));
